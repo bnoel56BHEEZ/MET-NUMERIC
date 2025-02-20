178c99ea5d42408703da71e1e3b9f44ff5822810
bnoel56BHEEZ <92136075+bnoel56BHEEZ@users.noreply.github.com>
2025-02-19 23:22:48 -0600

    Add files via upload
    este es un ejemplo de impresion de dos numeros en c

---
+++ printDosNumeros.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int num1 = 10;
+    int num2 = 20;
+    
+    std::cout << "El primer número es: " << num1 << std::endl;
+    std::cout << "El segundo número es: " << num2 << std::endl;
+    
+    return 0;
+}
