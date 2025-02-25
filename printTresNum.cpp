93ccab2bcf185fbaff18566844e969a0c0670658
bnoel56BHEEZ <92136075+bnoel56BHEEZ@users.noreply.github.com>
2025-02-24 22:52:36 -0600

    Add files via upload

---
+++ printTresNum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int num1 = 10;
+    int num2 = 20;
+    int num3 = 30;
+    
+    std::cout << "El primer número es: " << num1 << std::endl;
+    std::cout << "El segundo número es: " << num2 << std::endl;
+    std::cout << "El tercer número es: " << num2 << std::endl;
+    
+    return 0;
+}
